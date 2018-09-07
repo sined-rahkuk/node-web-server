b921e6079ff8c03bb7a24472976da7abf73f884c
Denys Kukhar <denys.kukhar@student.tuke.sk>
2018-09-07 16:46:10 +0200

    Initial commit

---
+++ node-web-server/server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+const app = express();
+app.set('view engine', 'hbs');
+app.use((req, resp, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}\n`;
+    console.log(log);
+    fs.appendFile('./logging/server.log', log, (err) => {
+        console.error(err);
+    })
+
+    next();
+});
+
+//uncomment for maintainig poproses
+// app.use((req, res) => res.render('maintenence.hbs'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, resp) => {
+    // resp.send('<hr><h1>hello express!</h1><hr>');
+    resp.render('home.hbs', {
+        pageTitle: 'Home page',
+        content: 'welcome to my brand new page',
+        owner: 'Denys Kukhar'
+    })
+});
+app.get('/about', (req, resp) => {
+    resp.render('about.hbs', {
+        pageTitle: 'About page',
+        owner: 'Denys Kukhar'
+    });
+});
+app.get('/bad', (req, resp) => {
+    resp.send({
+        errorMessage: "something went wrong or requested page does not exist!"
+    });
+});
+app.listen(3000, () => console.log('server is up on port 3000'));
